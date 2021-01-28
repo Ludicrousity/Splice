92c0c8c07623ffb7e2fcd0cc20ce12bf608da6ce
Ludicrousity <nathangtcooper@gmail.com>
2021-01-28 06:39:18 +0000

    Create abilities.ts

---
+++ abilities.ts
@@ -0,0 +1,8 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+splice: {
+		// Splice's type-changing itself is implemented in statuses.js
+		isPermanent: true,
+		name: "Splice",
+		rating: 4,
+		num: 50000,
+	},
